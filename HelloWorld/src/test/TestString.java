ce2ebb8fec64c3b3259c8cd14b99e7792386ce42
daman <daman@xps-1>
2026-01-19 22:40:11 +0900

    test string

---
+++ HelloWorld/src/test/TestString.java
@@ -0,0 +1,21 @@
+package test;
+
+public class TestString {
+
+	public static void main(String[] args) {
+		String json = """
+				{
+				"name": "John Doe",
+				"age": 30,
+				"email": "john@example.com"
+				}
+				""";
+		System.out.println(json);
+		
+		String name = "Alice";
+		int age = 30;
+		
+		//String message = STR."Hello,\{name}!";
+	}
+
+}
